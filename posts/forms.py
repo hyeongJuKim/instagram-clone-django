f815712208358455c7ee4a28b59a43b933e54698
hj <gudwndhkd@nate.com>
2019-07-06 12:34:58 +0900

    라인 정리

--- posts/forms.py
+++ posts/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserLoginForm(AuthenticationForm):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
