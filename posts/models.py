bd3dd5dd43dc1f2ceb93b35560b727eaeb29e4f0
hj <gudwndhkd@nate.com>
2019-07-25 21:32:59 +0900

    manage.py createuser, createsuperuser 되도록 수정

--- posts/models.py
+++ posts/models.py
@@ -6,24 +6,26 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, name, user_name, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
             email=self.normalize_email(email),
             name=name,
+            user_name=user_name,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, name):
+    def create_superuser(self, email, password, name, user_name):
         user = self.create_user(
             email,
             password=password,
             name=name,
+            user_name=user_name,
         )
         user.is_admin = True
         user.save(using=self._db)
