87a2df70d1248bd8c7f52d74741d3545de44bb37
hj <hjkim880527@gmail.com>
2019-10-17 23:23:38 +0900

    누락된 app 추가

--- instagram/settings.py
+++ instagram/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'ko-k'
 
 TIME_ZONE = 'Asia/Seoul'
 
