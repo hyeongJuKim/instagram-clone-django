fb118c2e4bfe7edcd4107d490e993a11454d64d5
hj <hjkim880527@gmail.com>
2019-10-23 23:43:12 +0900

    팝업 스크롤방지, 스타일 수정

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.staticfiles.finders import find
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + find(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
