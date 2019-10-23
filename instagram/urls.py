c3f9d058ec50e502b80db5dd3cfb5928ef833015
hj <hjkim880527@gmail.com>
2019-10-23 23:44:25 +0900

    잘못 수정한 부분 돌림

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,4 +8,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-] + find(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
