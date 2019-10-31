da7e5b4b05f502d91159eb36bd36ba46aa2a8100
hj <hjkim880527@gmail.com>
2019-10-31 23:51:52 +0900

    비밀번호 유효성검사 메시지 한글로 표시

--- posts/forms.py
+++ posts/forms.py
@@ -42,7 +42,7 @@ class UserCreationForm(forms.ModelForm):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError("비밀번호가 일치하지 않습니다.")
         return password2
 
     def save(self, commit=True):
