6fbc0105a793a29f8c850e8ce85e59c6d8da75c0
hj <hjkim880527@gmail.com>
2021-12-08 19:55:43 +0900

    admin site url 변경

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('manage-site/', admin.site.urls),
     path('', include('posts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
