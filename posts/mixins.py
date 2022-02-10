cbb5865afa8424d9e3bf483d7152b1552c377fce
hj <hjkim880527@gmail.com>
2022-02-10 23:42:39 +0900

    django 버전 업그레이드로 인해 사용불가 된 함수 재정의

--- posts/mixins.py
+++ posts/mixins.py
@@ -5,16 +5,19 @@ from posts import models
 
 class AjaxFormMixin(object):
 
+    def is_ajax(self, request):
+        return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
     def form_invalid(self, form):
         response = super(AjaxFormMixin, self).form_invalid(form)
-        if self.request.is_ajax():
+        if self.is_ajax(self.request):
             return JsonResponse(form.errors, status=400)
         else:
             return response
 
     def form_valid(self, form):
         response = super(AjaxFormMixin, self).form_valid(form)
-        if self.request.is_ajax():
+        if self.is_ajax(self.request):
             email = form.cleaned_data["email"]
             name = form.cleaned_data["name"]
             user_name = form.cleaned_data["user_name"]
@@ -26,3 +29,5 @@ class AjaxFormMixin(object):
             return JsonResponse(data)
         else:
             return response
+
+
